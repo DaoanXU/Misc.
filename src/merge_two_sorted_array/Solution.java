90cfa70353ee560ac5484e7223e1f6f2813e9b9e
Daoan XU <daoan.xu@gmail.com>
2013-03-21 02:31:24 -0400

    Bug Fixed

--- src/merge_two_sorted_array/Solution.java
+++ src/merge_two_sorted_array/Solution.java
@@ -1,24 +1,25 @@
-package merge_two_sorted_array
+package merge_two_sorted_array;
 
-public class Solution{
-    
-    public void merge(int[] arrayA, int[] arrayB, int tailA, int tailB){
+import java.util.Arrays;
+
+public class Solution {
+
+    public void merge(int[] arrayA, int[] arrayB, int tailA, int tailB) {
         int lastA = tailA + tailB + 1;
-        while(tailA > -1 && tailB > -1){
-            if(arrayA[tailA] > arrayB[tailB]){
+        while (tailA > -1 && tailB > -1) {
+            if (arrayA[tailA] > arrayB[tailB]) {
                 arrayA[lastA] = arrayA[tailA];
-                lastA --;
-                tailA --;
-            }
-            else{
-                arrayA[lastA] = arrayB[tailB]
-                lastA --;
-                tailB --;
+                lastA--;
+                tailA--;
+            } else {
+                arrayA[lastA] = arrayB[tailB];
+                lastA--;
+                tailB--;
             }
         }
-        
-        if(tailA == -1){
-            System.arraycopy(arrayA,0,arrayB,tailB);
-        }        
-    }    
+
+        if (tailA == -1) {
+            System.arraycopy(arrayB, 0, arrayA, 0, tailB + 1);
+        }
+    }
 }
